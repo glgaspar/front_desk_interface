cbdd5c40f5cf6c56560dfed0df53dda79a1569f5
Gustavo Gaspar <glgaspar@live.com>
2025-08-28 00:14:09 -0300

    should have imagined that

--- src/Components/Api/index.tsx
+++ src/Components/Api/index.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default function index() {
     return axios.create({
-        baseURL: process.env.API_URL,
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
         headers: {
         'Content-Type': 'application/json',
         },
