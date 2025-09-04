93a7e0378b67fa6ab136c460d530da4d1ffb03c0
Gustavo Gaspar <glgaspar@live.com>
2025-09-04 17:09:45 -0300

    customizing cards

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface CardProps {
 	children: React.ReactNode;
+	className?: string 
 }
 
-const Card: React.FC<CardProps> = ({ children }) => {
+const Card: React.FC<CardProps> = ({ className,children }) => {
 	return (
-		<div className="border-solid border-[#b3078b] border-[1px] rounded-xl p-3 m-1">
+		<div className={`border-solid border-[#b3078b] border-[1px] rounded-xl p-3 m-1 ${className}`}>
 			{children}
 		</div>
 	);
