e666fd56ba3e8a48c0b33829c711442478a1c437
Gustavo Gaspar <glgaspar@live.com>
2025-09-05 00:40:44 -0300

    none of that is useful

--- src/app/home/Interfaces/Features.ts
+++
@@ -1,5 +0,0 @@
-export default interface Featrues {
-    Img:  string
-	Name: string
-	Path: string
-}
--- src/app/paychecker/Interfaces/Bill.tsx
+++
@@ -1,8 +0,0 @@
-export default interface Bill {
-    id: number
-    description: string
-    expDay: number
-    path: string
-    lastDate: string
-    track: boolean
-}
